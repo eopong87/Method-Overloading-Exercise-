c83cc4730c48f4564d4c145a13cb81bd28b193a8
eopong87 <eopong87@gmail.com>
2023-07-12 14:34:47 -0400

    methods

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,62 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(0, 1, true));
+            Console.WriteLine();
+            Console.WriteLine(Add(5, 7, true));
+            Console.WriteLine();
+            Console.WriteLine(Add(-6, 3, false));
+            int answer1 = Add(5, 5);
+            Console.WriteLine(answer1);
+
+            decimal answer2 = Add(3.23m, 543.42m);
+            Console.WriteLine(answer2);
+
+            string answer3 = Add(10, 10, true);
+            Console.WriteLine(answer3);
+        }
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+        public static string Add(int num1, int num2, bool IsMoney)
+        {
+            var sum = num1 + num2;
+
+            
+            if (IsMoney == true && sum == 1)
+            {
+
+                return $"{sum} dollar";
+
+            }
+
+            else if (IsMoney == true)
+            {
+
+
+
+                return $"{sum} dollars";
+            }
+            else
+
+            {
+                return $"{sum}";
+
+
+
+            }
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
         }
     }
 }
+
+
+
+
+
+
